d8ed4b64f6edceb9a6fae72f3a9e6cff1b3a6627
Fabrício Canedo <cron0s@localhost.localdomain>
2024-08-18 02:36:11 -0300

    adds the avoid repeats feature

--- main.c
+++ main.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include <time.h>
 #include <argp.h>
 
 static char doc[] = "Simple password generator.";
-static char args_doc[] = "";
+static char args_doc[] = "LENGTH COMPLEXITY";
 
 static struct argp_option options[] = {
-    {"length", 'l', "NUMBER", 0, "Set password length"},
-    {"complexity", 'c', "NUMBER", 0, "Set complexity level"},
+    {"avoid-repeats", 'a', 0, 0, "Avoid repeated characters"},
     {0}
 };
 
 struct arguments {
     int length;
     int complexity;
+    int avoid_repeats;
 };
 
 static error_t parse_opt(int key, char *arg, struct argp_state *state) {
@@ -25,19 +26,35 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
     int complexity;
 
     switch (key) {
-        case 'l':
-            length = atoi(arg);
-            if (length < 8 || length > 50) {
-                argp_error(state, "Password length must be between 8 and 50.");
-            }
-            arguments->length = length;
+        case 'a':
+            arguments->avoid_repeats = true;
             break;
-        case 'c':
-            complexity = atoi(arg);
-            if (complexity < 1 || complexity > 4) {
-                argp_error(state, "Complexity level must be between 1 and 4.");
+        case ARGP_KEY_ARG:
+            if (state->arg_num >= 2) {
+                argp_usage(state);
             }
-            arguments->complexity = complexity;
+
+            if (state->arg_num == 0) {
+                length = atoi(arg);
+                if (length < 8 || length > 50) {
+                    argp_error(state, "Password length must be between 8 and 50.");
+                }
+                arguments->length = length;
+            }
+
+            if (state->arg_num == 1) {
+                complexity = atoi(arg);
+                if (complexity < 1 || complexity > 4) {
+                    argp_error(state, "Complexity level must be between 1 and 4.");
+                }
+                arguments->complexity = complexity;
+            }
+
+            break;
+        case ARGP_KEY_END:
+            if (state->arg_num < 2) {
+                argp_usage(state);
+            }
             break;
         default:
             return ARGP_ERR_UNKNOWN;
@@ -48,7 +65,7 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
 
 static struct argp argp = {options, parse_opt, args_doc, doc};
 
-char *generate_password(int length, int complexity) {
+char *generate_password(int length, int complexity, bool avoid_repeats) {
     char *password = (char *)calloc(length + 1, sizeof(char));
 
     if (!password) {
@@ -77,8 +94,24 @@ char *generate_password(int length, int complexity) {
 
     srand(time(NULL));
 
-    for (int index = 0; index < length; index++) {
-        password[index] = characters[rand() % characters_total];
+    if (avoid_repeats) {
+        int index = 0;
+        char random_char;
+
+        while (index < length) {
+            random_char = characters[rand() % characters_total];
+
+            if (strchr(password, random_char)) {
+                continue;
+            }
+
+            password[index] = random_char;
+            index++;
+        }
+    } else {
+        for (int index = 0; index < length; index++) {
+            password[index] = characters[rand() % characters_total];
+        }
     }
 
     return password;
@@ -87,12 +120,11 @@ char *generate_password(int length, int complexity) {
 void main(int argc, char **argv) {
     struct arguments arguments;
 
-    arguments.length = 8;
-    arguments.complexity = 1;
+    arguments.avoid_repeats = false;
 
     argp_parse(&argp, argc, argv, 0, 0, &arguments);
 
-    char *password = generate_password(arguments.length, arguments.complexity);
+    char *password = generate_password(arguments.length, arguments.complexity, arguments.avoid_repeats);
 
     puts(password);
 
